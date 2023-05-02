e4e456f4c2e9905bba0dcab321fe9cac9c07daaf
Mohammad-Shahin23 <mohammad.shahin2221@gmail.com>
2023-05-02 15:46:28 +0000

    zip finished and completed kth function

--- linked_list1/linked_list.py
+++ linked_list1/linked_list.py
@@ -7,8 +7,20 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def insert(self, value):
+    def __str__(self):
+        return self.to_string()
+    
+    def append(self,value):
+        """Adds a new node with that value to the end of the list 
+        Args:
+            value: value of the new node
+            
+            Returns: nothing
+            
+            Raises: nothing"""
+
         node = Node(value)
+        
         if self.head is None:
             self.head = node
         else:
@@ -17,42 +29,179 @@ class LinkedList:
                 current = current.next
             current.next = node
 
-    def includes(self, value):
+    def insert(self, data):
+        """Inserts a new node with that value before the first node of the list
+        Args:
+            value: value of the new node
+            
+            Returns: nothing
+            
+            Raises: nothing """
+        newNode = Node(data)
+        newNode.next = self.head
+        self.head = newNode
+
+    def includes(self,data):
+        """Indicates whether that value exists as a Node’s value somewhere within the list.
+        Args:
+            value: value of the new node
+            
+            Returns: Boolean
+            
+            Raises: nothing """
         current = self.head
         while current is not None:
-            if current.value == value:
+            if current.value == data:
                 return True
             current = current.next
         return False
 
-    def __str__(self):
+    def to_string(self):
+        """Returns: a string representing all the values in the Linked List, formatted as:
+        "{ a } -> { b } -> { c } -> NULL"
+        Args:
+            value: value of the new node
+            
+            Returns: string
+            
+            Raises: nothing """
         current = self.head
-        result = ''
+        string = ""
         while current is not None:
-            result += f'{{ {current.value} }} -> '
+            string += f"{{ {current.value} }} -> "
             current = current.next
-        result += 'NULL'
-        return result
+        string += "NULL"
+        return string
     
-    if __name__ == '__main__':
-      linked_list = LinkedList()
+    def insert_before(self, value, new_value):
+        """
+        Inserts a new node containing the given new_value before the first occurrence of the given value in the linked list.
+        Parameters:
+        ----------
+        value : any
+            The value to search for in the linked list.
+        new_value : any
+            The value to be stored in the new node.
+        Returns:
+        -------
+        None
+        """
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            return
+        if self.head.value == value:
+            new_node.next = self.head
+            self.head = new_node
+            return
+        current_node = self.head
+        while current_node.next:
+            if current_node.next.value == value:
+                new_node.next = current_node.next
+                current_node.next = new_node
+                return
+            current_node = current_node.next
 
-      linked_list.insert('A')
-      linked_list.insert('B')
-      linked_list.insert('C')
-      # # linked_list.append('D')
-      # # linked_list.append('E')
-      linked_list.insert_after('A','f')
-      
-     
-      
-      print(linked_list)
-      print(linked_list.count)
+    def insert_after(self, value, new_value):
+        """
+        Inserts a new node containing the given new_value after the first occurrence of the given value in the linked list.
+        Parameters:
+        ----------
+        value : any
+            The value to search for in the linked list.
+        new_value : any
+            The value to be stored in the new node.
+        Returns:
+        -------
+        None
+        """
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            return
+        current_node = self.head
+        while current_node:
+            if current_node.value == value:
+                new_node.next = current_node.next
+                current_node.next = new_node
+                return
+            current_node = current_node.next
 
-      print(linked_list.kthFromEnd(4))
-      
+    def delete(self, value):
+        """
+        Deletes the first node containing the given value from the linked list, if it exists.
+        Parameters:
+        ----------
+        value : any
+            The value to search for and delete from the linked list.
+        Returns:
+        -------
+        None
+        """
+        current_node = self.head
+        if current_node and current_node.value == value:
+            self.head = current_node.next
+            current_node = None
+            return
 
-    def zipLists(list1: 'LinkedList', list2: 'LinkedList') -> 'LinkedList':
+        previous_node = None
+        while current_node and current_node.value != value:
+            previous_node = current_node
+            current_node = current_node.next
+
+        if current_node is None:
+            return
+
+        previous_node.next = current_node.next
+        current_node = None
+
+    def kth_from_end(self, k):
+        if k <= 0:
+            return None
+
+        # Get the length of the linked list
+        length = 0
+        current = self.head
+        while current:
+            length += 1
+            current = current.next
+
+        if k > length:
+            return None
+
+        # Calculate the position of the kth node from the beginning
+        position = length - k
+
+        # Traverse the linked list to find the kth node from the beginning
+        current = self.head
+        for i in range(position):
+            current = current.next
+
+        return current.value
+
+    def find_middle(self):
+        if not self.head:
+            return None
+
+        slow = self.head
+        fast = self.head
+
+        # Traverse the linked list using two pointers,
+        # one moving at half the speed of the other.
+        # When the fast pointer reaches the end of the
+        # list, the slow pointer will be at the middle node.
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow.value
+
+    
+    
+    
+
+    @staticmethod
+    def zipLists(list1:'LinkedList', list2:'LinkedList'):
         """
         Zips two linked lists together into one linked list, alternating between nodes from each list.
         Parameters:
@@ -66,29 +215,21 @@ class LinkedList:
         LinkedList
             A new linked list containing the zipped nodes.
         """
-        if not list1.head:
+        
+        
+        if list1.head is None:
             return list2
-        if not list2.head:
+        if list2.head is None:
             return list1
-
-        current1 = list1.head
-        current2 = list2.head
-        new_list = LinkedList()
-
-        while current1 and current2:
-            new_list.insert(current1.value)
-            new_list.insert(current2.value)
-            current1 = current1.next
-            current2 = current2.next
-
-        if current1:
-            while current1:
-                new_list.insert(current1.value)
-                current1 = current1.next
-
-        if current2:
-            while current2:
-                new_list.insert(current2.value)
-                current2 = current2.next
-
-        return new_list
+        list1_current = list1.head
+        list2_current = list2.head
+        while list1_current and list2_current:
+            list1_next = list1_current.next
+            list2_next = list2_current.next
+            list1_current.next = list2_current
+            if list1_next is None:
+                break
+            list2_current.next = list1_next
+            list1_current = list1_next
+            list2_current = list2_next
+        return list1
--- linked_list1/test_zip.py
+++ linked_list1/test_zip.py
@@ -1,56 +1,52 @@
+import pytest
 from linked_list import LinkedList, Node
 
 
+def test_zipLists1():
+    linked_list1 = LinkedList()
+    linked_list1.append(1)
+    linked_list1.append(3)
+    linked_list1.append(2)
+    linked_list2 = LinkedList()
+    linked_list2.append(5)
+    linked_list2.append(9)
+    linked_list2.append(4)
+    zipList = linked_list1.zipLists(linked_list1, linked_list2)
+    assert zipList.to_string() == "{ 1 } -> { 5 } -> { 3 } -> { 9 } -> { 2 } -> { 4 } -> NULL"
 
-def test_zipLists():
-    # create two linked lists
-    list1 = LinkedList()
-    list1.insert(2)
-    list1.insert(3)
-    list1.insert(1)
+def test_zipLists2():
+    linked_list1 = LinkedList()
+    linked_list1.append(1)
+    linked_list1.append(3)
+    linked_list2 = LinkedList()
+    linked_list2.append(5)
+    linked_list2.append(9)
+    linked_list2.append(4)
+    zipList = linked_list1.zipLists(linked_list1, linked_list2)
+    assert zipList.to_string() == "{ 1 } -> { 5 } -> { 3 } -> { 9 } -> { 4 } -> NULL"
 
-    list2 = LinkedList()
-    list2.insert(4)
-    list2.insert(9)
-    list2.insert(5)
+def test_zipLists3():
+    linked_list1 = LinkedList()
+    linked_list1.append(1)
+    linked_list1.append(3)
+    linked_list1.append(2)
+    linked_list2 = LinkedList()
+    linked_list2.append(5)
+    linked_list2.append(9)
+    zipList = linked_list1.zipLists(linked_list1, linked_list2)
+    assert zipList.to_string() == "{ 1 } -> { 5 } -> { 3 } -> { 9 } -> { 2 } -> NULL"
 
-    # zip the two linked lists
-    zipped_list = LinkedList.zipLists(list1, list2)
+def test_zipLists_one_list_is_null():
+    linked_list1 = LinkedList()
+    linked_list1.append(1)
+    linked_list1.append(3)
+    linked_list1.append(2)
+    linked_list2 = LinkedList()
+    zipList = linked_list1.zipLists(linked_list1, linked_list2)
+    assert zipList.to_string() == "{ 1 } -> { 3 } -> { 2 } -> NULL"
 
-    # check the result
-    expected_output = "{ 1 } -> { 5 } -> { 3 } -> { 9 } -> { 2 } -> { 4 } -> NULL"
-    assert zipped_list.to_string() == expected_output
-
-    # test with different lengths
-    list1 = LinkedList()
-    list1.insert(2)
-    list1.insert(3)
-    list1.insert(1)
-
-    list2 = LinkedList()
-    list2.insert(4)
-    list2.insert(9)
-
-    zipped_list = LinkedList.zipLists(list1, list2)
-    expected_output = "{ 1 } -> { 4 } -> { 3 } -> { 9 } -> { 2 } -> NULL"
-    assert zipped_list.to_string() == expected_output
-
-    # test with empty lists
-    list1 = LinkedList()
-    list2 = LinkedList()
-
-    zipped_list = LinkedList.zipLists(list1, list2)
-    expected_output = "NULL"
-    assert zipped_list.to_string() == expected_output
-
-    # test with one empty list
-    list1 = LinkedList()
-    list1.insert(2)
-    list1.insert(3)
-    list1.insert(1)
-
-    list2 = LinkedList()
-
-    zipped_list = LinkedList.zipLists(list1, list2)
-    expected_output = "{ 1 } -> { 3 } -> { 2 } -> NULL"
-    assert zipped_list.to_string() == expected_output
+def test_zipLists_both_lists_are_null():
+    linked_list1 = LinkedList()
+    linked_list2 = LinkedList()
+    zipList = linked_list1.zipLists(linked_list1, linked_list2)
+    assert zipList.to_string() == "NULL"
