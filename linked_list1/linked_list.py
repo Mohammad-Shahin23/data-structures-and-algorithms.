9010f4cf689d0d409143b812449326e0c9563016
Mohammad-Shahin23 <mohammad.shahin2221@gmail.com>
2023-05-06 22:50:30 +0000

    done some changes

--- linked_list1/linked_list.py
+++ linked_list1/linked_list.py
@@ -155,81 +155,107 @@ class LinkedList:
         previous_node.next = current_node.next
         current_node = None
 
-    def kth_from_end(self, k):
-        if k <= 0:
-            return None
+    def kth_From_End(self,k):
+        if self.head is None:
+            return "Error : the linked list is empty"
+        elif k >= LinkedList.count:
+            return "Error : Your input can't be more than the length"  
+        elif k < 0 :
+            return "Error : only positive numbers are accepted!"
+        elif LinkedList.count == 1:
+            return self.head.value
+        else:        
+         pointer_one = self.head
+         pointer_two = self.head
 
-        # Get the length of the linked list
-        length = 0
-        current = self.head
-        while current:
-            length += 1
-            current = current.next
+         for i in range(k):
+            pointer_two = pointer_two.next
 
-        if k > length:
-            return None
+         while pointer_two.next is not None:
+           pointer_one = pointer_one.next
+           pointer_two = pointer_two.next
 
-        # Calculate the position of the kth node from the beginning
-        position = length - k
+         return pointer_one.value
 
-        # Traverse the linked list to find the kth node from the beginning
-        current = self.head
-        for i in range(position):
-            current = current.next
+    # def kth_from_end(self, k):
+    #     if k <= 0:
+    #         return None
 
-        return current.value
+    #     # Get the length of the linked list
+    #     length = 0
+    #     current = self.head
+    #     while current:
+    #         length += 1
+    #         current = current.next
 
-    def find_middle(self):
-        if not self.head:
-            return None
+    #     if k > length:
+    #         return None
 
-        slow = self.head
-        fast = self.head
+    #     # Calculate the position of the kth node from the beginning
+    #     position = length - k
 
-        # Traverse the linked list using two pointers,
-        # one moving at half the speed of the other.
-        # When the fast pointer reaches the end of the
-        # list, the slow pointer will be at the middle node.
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+    #     # Traverse the linked list to find the kth node from the beginning
+    #     current = self.head
+    #     for i in range(position):
+    #         current = current.next
 
-        return slow.value
+    #     return current.value
+
+    # def find_middle(self):
+    #     if not self.head:
+    #         return None
+
+    #     slow = self.head
+    #     fast = self.head
+
+    #     # Traverse the linked list using two pointers,
+    #     # one moving at half the speed of the other.
+    #     # When the fast pointer reaches the end of the
+    #     # list, the slow pointer will be at the middle node.
+    #     while fast and fast.next:
+    #         slow = slow.next
+    #         fast = fast.next.next
+
+    #     return slow.value
 
     
     
     
 
-    @staticmethod
-    def zipLists(list1:'LinkedList', list2:'LinkedList'):
-        """
-        Zips two linked lists together into one linked list, alternating between nodes from each list.
-        Parameters:
-        ----------
-        list1 : LinkedList
-            The first linked list to zip.
-        list2 : LinkedList
-            The second linked list to zip.
-        Returns:
-        -------
-        LinkedList
-            A new linked list containing the zipped nodes.
-        """
+    # @staticmethod
+    # def zipLists(list1:'LinkedList', list2:'LinkedList'):
+    #     """
+    #     Zips two linked lists together into one linked list, alternating between nodes from each list.
+    #     Parameters:
+    #     ----------
+    #     list1 : LinkedList
+    #         The first linked list to zip.
+    #     list2 : LinkedList
+    #         The second linked list to zip.
+    #     Returns:
+    #     -------
+    #     LinkedList
+    #         A new linked list containing the zipped nodes.
+    #     """
         
         
-        if list1.head is None:
-            return list2
-        if list2.head is None:
-            return list1
-        list1_current = list1.head
-        list2_current = list2.head
-        while list1_current and list2_current:
-            list1_next = list1_current.next
-            list2_next = list2_current.next
-            list1_current.next = list2_current
-            if list1_next is None:
-                break
-            list2_current.next = list1_next
-            list1_current = list1_next
-            list2_current = list2_next
-        return list1
+    #     if list1.head is None:
+    #         return list2
+    #     if list2.head is None:
+    #         return list1
+    #     list1_current = list1.head
+    #     list2_current = list2.head
+    #     while list1_current and list2_current:
+    #         list1_next = list1_current.next
+    #         list2_next = list2_current.next
+    #         list1_current.next = list2_current
+    #         if list1_next is None:
+    #             break
+    #         list2_current.next = list1_next
+    #         list1_current = list1_next
+    #         list2_current = list2_next
+    #     return list1
+
+
+
+
