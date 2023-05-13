ff67a8b7b57e13c15d800a1ad887686cf1711238
Mohammad-Shahin23 <mohammad.shahin2221@gmail.com>
2023-05-13 18:47:57 +0000

    done

--- stack_and_queue/tests/test_pseudo.py
+++ stack_and_queue/tests/test_pseudo.py
@@ -3,23 +3,23 @@ from stack_and_queue.pseudo import PseudoQueue
 def test_enqueue_single_element():
     queue = PseudoQueue()
     queue.enqueue(5)
-    assert queue.stack1.stack == [5]
-    assert queue.stack2.stack == []
+    assert queue.stackA.stack == [5]
+    assert queue.stackB.stack == []
 
 def test_enqueue_multiple_elements():
     queue = PseudoQueue()
     queue.enqueue(5)
     queue.enqueue(10)
     queue.enqueue(15)
-    assert queue.stack1.stack == [5, 10, 15]
-    assert queue.stack2.stack == []
+    assert queue.stackA.stack == [5, 10, 15]
+    assert queue.stackB.stack == []
 
 def test_dequeue_single_element():
     queue = PseudoQueue()
     queue.enqueue(5)
     assert queue.dequeue() == 5
-    assert queue.stack1.stack == []
-    assert queue.stack2.stack == []
+    assert queue.stackA.stack == []
+    assert queue.stackB.stack == []
 
 def test_dequeue_multiple_elements():
     queue = PseudoQueue()
@@ -29,8 +29,8 @@ def test_dequeue_multiple_elements():
     assert queue.dequeue() == 5
     assert queue.dequeue() == 10
     assert queue.dequeue() == 15
-    assert queue.stack1.stack == []
-    assert queue.stack2.stack == []
+    assert queue.stackA.stack == []
+    assert queue.stackB.stack == []
 
 
 
